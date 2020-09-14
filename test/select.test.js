3a5c24e438959497b5181ec3067f2688c4a619df
web-padawan <iamkulykov@gmail.com>
2020-09-14 14:50:19 +0300

    Convert more tests to WTR [skip ci]

--- test/select.test.js
+++ test/select.test.js
@@ -1,7 +1,8 @@
 import { expect } from '@esm-bundle/chai';
+import sinon from 'sinon';
 import { fixture, html, nextFrame } from '@open-wc/testing-helpers';
 import { render } from 'lit-html';
-import { keyDownOn } from '@polymer/iron-test-helpers/mock-interactions.js';
+import { keyDownOn, keyboardEventFor } from '@polymer/iron-test-helpers/mock-interactions.js';
 import '@vaadin/vaadin-list-box/vaadin-list-box.js';
 import '@vaadin/vaadin-item/vaadin-item.js';
 import './mock-item.js';
@@ -17,15 +18,23 @@ function arrowDown(target) {
 }
 
 function space(target) {
-  MockInteractions.keyDownOn(target, 32, [], ' ');
+  keyDownOn(target, 32, [], ' ');
 }
 
 function enter(target) {
-  MockInteractions.keyDownOn(target, 13, [], 'Enter');
+  keyDownOn(target, 13, [], 'Enter');
 }
 
 function esc(target) {
-  MockInteractions.keyDownOn(target, 27, [], 'Escape');
+  keyDownOn(target, 27, [], 'Escape');
+}
+
+function keyDownChar(target, letter, modifier) {
+  keyDownOn(target, letter.charCodeAt(0), modifier, letter);
+}
+
+function tab(target) {
+  keyDownOn(target, 9, [], 'Tab');
 }
 
 describe('vaadin-select', () => {
@@ -189,6 +198,22 @@ describe('vaadin-select', () => {
       });
     });
 
+    describe('keyboard selection', () => {
+      it('should select items when alphanumeric keys are pressed', () => {
+        select.opened = false;
+        expect(select._menuElement.selected).to.be.equal(2);
+        keyDownChar(input, 'o');
+        keyDownChar(input, 'p');
+        keyDownChar(input, 't');
+        expect(select._menuElement.selected).to.be.equal(0);
+        keyDownChar(input, 'i');
+        keyDownChar(input, 'o');
+        keyDownChar(input, 'n');
+        keyDownChar(input, '2');
+        expect(select._menuElement.selected).to.be.equal(1);
+      });
+    });
+
     describe('opening the overlay', () => {
       it('should keep synchronized opened properties in overlay and select', () => {
         select.opened = true;
@@ -263,6 +288,91 @@ describe('vaadin-select', () => {
       });
     });
 
+    describe('overlay opened', () => {
+      let menu;
+
+      beforeEach(async () => {
+        menu = select._menuElement;
+        await nextFrame();
+        select.focus();
+        enter(input);
+      });
+
+      it('should close the select on selecting the same value', () => {
+        enter(input);
+        expect(select._overlayElement.opened).to.be.true;
+        select._items[0].dispatchEvent(new CustomEvent('click', { bubbles: true }));
+        expect(select._overlayElement.opened).to.be.false;
+      });
+
+      it('should focus the input on selecting value and closing the overlay', () => {
+        const focusedSpy = sinon.spy();
+        select.focusElement.focus = focusedSpy;
+
+        menu.selected = 1;
+        expect(select.value).to.be.equal(select._items[menu.selected].value);
+        expect(select._overlayElement.opened).to.be.false;
+        expect(focusedSpy.called).to.be.true;
+      });
+
+      it('should remove focused state from the input on closing the overlay if phone', () => {
+        select._phone = true;
+        menu.selected = 1;
+        expect(select.hasAttribute('focused')).to.be.false;
+        expect(input.hasAttribute('focused')).to.be.false;
+      });
+
+      it('should not focus the input on closing the overlay if phone', () => {
+        const focusedSpy = sinon.spy();
+        select.focusElement.focus = focusedSpy;
+
+        select._phone = true;
+        menu.selected = 1;
+        expect(focusedSpy.called).to.be.false;
+      });
+
+      it('should focus the input before moving the focus to next selectable element', () => {
+        const focusedSpy = sinon.spy();
+        select.focusElement.focus = focusedSpy;
+
+        tab(menu);
+        expect(focusedSpy.called).to.be.true;
+      });
+
+      it('should hide native input when a item is selected, and it has content', () => {
+        menu.selected = 2;
+        const slotExists = select._valueElement.slot === 'input';
+        expect(slotExists).to.be.true;
+      });
+
+      it('should toggle native native input visibility when the content of the selected item is empty', () => {
+        menu.selected = 2;
+        menu.selected = 3;
+        expect(select._valueElement.slot).to.equal('');
+      });
+
+      it('should show native input when label of the selected items is an empty string', () => {
+        menu.selected = 4;
+        expect(select._valueElement.slot).to.equal('');
+      });
+
+      it('should close the overlay when clicking on the overlay', () => {
+        select.opened = false;
+        select._overlayElement.click();
+        expect(select.opened).to.be.false;
+      });
+
+      // iOS Safari has incorrect viewport height when navigation bar is
+      // visible in landscape orientation. This is workarounded by exposing
+      // --vaadin-overlay-viewport-bottom in <vaadin-overlay>.
+      it('should support --vaadin-overlay-viewport-bottom CSS property', () => {
+        const overlay = select._overlayElement;
+        overlay.setAttribute('phone', '');
+        overlay.style.setProperty('--vaadin-overlay-viewport-bottom', '50px');
+        expect(getComputedStyle(overlay).getPropertyValue('bottom')).to.equal('50px');
+      });
+    });
+
     describe('has-value attribute on text-field', () => {
       it('should not be set by default', () => {
         expect(select._inputElement.getAttribute('has-value')).to.be.null;
@@ -303,6 +413,191 @@ describe('vaadin-select', () => {
         expect(select._overlayElement.opened).to.be.false;
       });
     });
+
+    describe('focus', () => {
+      it('should be focusable', () => {
+        select.focus();
+        expect(select.hasAttribute('focused')).to.be.true;
+        expect(input.hasAttribute('focused')).to.be.true;
+      });
+
+      it('should not focus the input by default', () => {
+        expect(select.focusElement.hasAttribute('focused')).not.to.be.ok;
+      });
+
+      it('should set tabindex attribute to -1 on the native input', () => {
+        expect(select._nativeInput.getAttribute('tabindex')).to.be.equal('-1');
+      });
+
+      it('should focus the next focusable element when tabbing', () => {
+        select.focus();
+
+        // Tabbing does not natively move the focus, hence we only can check that the event is not prevented
+        const ev = keyboardEventFor(9, [], 'Tab');
+        input.dispatchEvent(ev);
+        expect(ev.defaultPrevented).to.be.false;
+      });
+
+      it('should focus the previous element when shift tabbing', () => {
+        select.focus();
+
+        // Tabbing does not natively move the focus, hence we only can check that the event is not prevented
+        const ev = keyboardEventFor(9, ['shift'], 'Tab');
+        input.dispatchEvent(ev);
+        expect(ev.defaultPrevented).to.be.false;
+      });
+
+      it('should set pointer-events to none on the native input to fix iOS Zooming', () => {
+        select.value = 'nomatch';
+        const nativeInput = select._inputElement.shadowRoot.querySelector('input');
+        expect(getComputedStyle(nativeInput)['pointer-events']).to.equal('none');
+      });
+    });
+
+    describe('focus when overlay opened', () => {
+      it('should keep focused state after opening overlay when focused', () => {
+        select.focus();
+        select.opened = true;
+        expect(select.hasAttribute('focused')).to.be.true;
+        expect(input.hasAttribute('focused')).to.be.true;
+      });
+
+      it('should not set focused state after opening overlay if not focused', () => {
+        select.opened = true;
+        expect(select.hasAttribute('focused')).to.be.false;
+        expect(input.hasAttribute('focused')).to.be.false;
+      });
+    });
+
+    describe('validation', () => {
+      it('should set invalid to true when is required but there is no value', () => {
+        expect(select.invalid).to.be.false;
+        select.setAttribute('required', '');
+
+        enter(input);
+        esc(input);
+        expect(select.invalid).to.be.true;
+      });
+
+      it('should not set invalid to true when is required, there is no value, but is disabled', () => {
+        expect(select.invalid).to.be.false;
+        select.setAttribute('required', '');
+        select.setAttribute('disabled', '');
+
+        select.validate();
+        expect(select.invalid).to.be.false;
+      });
+
+      it('should validate when closing the overlay', () => {
+        const spy = sinon.spy();
+        select.validate = spy;
+        select.opened = true;
+
+        select.opened = false;
+        expect(spy.called).to.be.true;
+      });
+
+      it('should validate when blurring', () => {
+        const spy = sinon.spy();
+        select.validate = spy;
+        select.blur();
+
+        expect(spy.called).to.be.true;
+      });
+
+      it('should validate when setting value', () => {
+        const spy = sinon.spy();
+        select.validate = spy;
+        select.value = 'v2';
+        expect(spy.callCount).to.be.equal(1);
+        select.value = '';
+        expect(spy.callCount).to.be.equal(2);
+      });
+
+      it('should show error message when validated.', () => {
+        expect(select.invalid).to.be.false;
+        select.errorMessage = 'Please choose one option';
+        select.setAttribute('required', '');
+        select.validate();
+
+        expect(select.invalid).to.be.true;
+        expect(input.invalid).to.be.true;
+        expect(input.errorMessage).to.be.equal(select.errorMessage);
+      });
+    });
+
+    describe('initial validation', () => {
+      let spy;
+
+      beforeEach(async () => {
+        select.required = true;
+        spy = sinon.spy();
+        select.validate = spy;
+        await nextFrame();
+      });
+
+      it('should not validate the initial empty value', () => {
+        expect(spy.called).to.be.false;
+      });
+    });
+
+    describe('`change` event', () => {
+      let menu, changeSpy;
+
+      beforeEach(async () => {
+        menu = select._menuElement;
+        await nextFrame();
+        changeSpy = sinon.spy();
+        select.addEventListener('change', changeSpy);
+      });
+
+      it('should not fire `change` event when programmatically opened and selected changed', () => {
+        select.opened = true;
+        menu.selected = 1;
+        select.opened = false;
+        expect(changeSpy.called).to.be.false;
+      });
+
+      it('should not fire `change` event when programmatically opened and value changed', () => {
+        select.opened = true;
+        select.value = 'Option 1';
+        expect(changeSpy.called).to.be.false;
+      });
+
+      it('should not fire `change` event when committing a value programmatically', () => {
+        menu.selected = 1;
+        select.value = 'Option 1';
+        expect(changeSpy.called).to.be.false;
+      });
+
+      it('should stop input `change` event from bubbling', () => {
+        select._inputElement.dispatchEvent(new CustomEvent('change'));
+        expect(changeSpy.called).to.be.false;
+      });
+
+      it('should fire `change` event when value changes when alphanumeric keys are pressed', () => {
+        keyDownChar(input, 'o');
+        keyDownChar(input, 'p');
+        keyDownChar(input, 't');
+        expect(changeSpy.callCount).to.equal(1);
+      });
+
+      it('should fire `change` event when value changes by user clicking the item', () => {
+        select.opened = true;
+        menu.firstElementChild.click();
+        expect(changeSpy.callCount).to.equal(1);
+      });
+
+      it('should fire `change` event when value changes by user selecting item with keyboard', () => {
+        select.opened = true;
+        arrowUp(menu);
+
+        const secondOption = menu.querySelector('[value="v2"]');
+        MockInteractions.keyDownOn(secondOption, 13, [], 'Enter');
+        MockInteractions.keyUpOn(secondOption, 13, [], 'Enter');
+        expect(changeSpy.callCount).to.equal(1);
+      });
+    });
   });
 
   describe('with value', () => {
@@ -334,8 +629,6 @@ describe('vaadin-select', () => {
   });
 
   describe('with theme attribute', () => {
-    let select;
-
     beforeEach(async () => {
       select = await fixture(html`<vaadin-select theme="foo"></vaadin-select>`);
     });
@@ -364,6 +657,26 @@ describe('vaadin-select', () => {
       const select = container.querySelector('vaadin-select');
       expect(window.getComputedStyle(container).width).to.eql('500px');
       expect(parseFloat(window.getComputedStyle(select).width)).to.eql(500);
+    });
+  });
+
+  describe('with helper text', () => {
+    beforeEach(async () => {
+      select = await fixture(html`<vaadin-select></vaadin-select>`);
+    });
+
+    it('should display the helper text when slotted helper available', async () => {
+      const div = document.createElement('div');
+      div.textContent = 'foo';
+      div.setAttribute('slot', 'helper');
+      select.appendChild(div);
+      await nextFrame();
+      expect(select._inputElement.querySelector('[slot="helper"]').assignedNodes()[0].textContent).to.eql('foo');
+    });
+
+    it('should display the helper text when provided', () => {
+      select.helperText = 'Foo';
+      expect(select._inputElement.helperText).to.equal(select.helperText);
     });
   });
 });
