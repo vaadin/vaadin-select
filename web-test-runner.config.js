193d3d2ed3c730ed72006f641d4d7ef3cac5e3be
web-padawan <iamkulykov@gmail.com>
2020-09-15 15:05:41 +0300

    Fix config for GitHub actions

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,22 +1,22 @@
 const { createSauceLabsLauncher } = require('@web/test-runner-saucelabs');
 
-const sauceLabsLauncher = createSauceLabsLauncher({
-  user: process.env.SAUCE_USERNAME,
-  key: process.env.SAUCE_ACCESS_KEY
-});
-
 const config = {
   nodeResolve: true
 };
 
-const sharedCapabilities = {
-  'sauce:options': {
-    name: 'vaadin-select unit tests',
-    build: `build ${process.env.TRAVIS_JOB_NUMBER || ''}`
-  }
-};
-
 if (process.env.TEST_ENV === 'sauce') {
+  const sauceLabsLauncher = createSauceLabsLauncher({
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY
+  });
+
+  const sharedCapabilities = {
+    'sauce:options': {
+      name: 'vaadin-select unit tests',
+      build: `build ${process.env.TRAVIS_JOB_NUMBER || ''}`
+    }
+  };
+
   config.concurrency = 1;
   config.browsers = [
     sauceLabsLauncher({
